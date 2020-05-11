95471de5f85e892b626a4df0f8b02355835f4550
liyanan <liyanan001@yeah.net>
2020-05-11 16:23:18 +0800

    test Ioc

--- Ioc.hpp
+++ Ioc.hpp
@@ -24,7 +24,7 @@ public:
 	T* Resolve(const string& strKey, Args... args)
 	{
 		if (m_creatorMap.find(strKey) == m_creatorMap.end())
-			returnnullptr;
+			return nullptr;
 
 		Any resolver = m_creatorMap[strKey];
 		std::function<T* (Args...)> function = resolver.AnyCast<std::function<T* (Args...)>>();
@@ -53,64 +53,4 @@ private:
 private:
 	unordered_map<string, Any> m_creatorMap;
 };
-/*test code
-struct Base
-{
-	virtual void Func(){}
-	virtual ~Base(){}
-};
 
-struct DerivedB : public Base
-{
-	DerivedB(int a, double b):m_a(a),m_b(b)
-	{
-	}
-	void Func()override
-	{
-		cout<<m_a+m_b<<endl;
-	}
-private:
-	int m_a;
-	double m_b;
-};
-
-struct DerivedC : public Base
-{
-};
-
-struct A
-{
-	A(Base * ptr) :m_ptr(ptr)
-	{		
-	}
-
-	void Func()
-	{
-		m_ptr->Func();
-	}
-	
-	~A()
-	{
-		if(m_ptr!=nullptr)
-		{
-			delete m_ptr;
-			m_ptr = nullptr;
-		}
-	}
-
-private:
-Base * m_ptr;
-};
-
-void TestIoc()
-{
-	IocContainer ioc;
-	ioc.RegisterType<A, DerivedC>(“C”);      //配置依赖关系
-	auto c = ioc.ResolveShared<A>(“C”);
-
-	ioc.RegisterType<A, DerivedB, int, double>(“C”);   //注册时要注意DerivedB的参数int和double
-	auto b = ioc.ResolveShared<A>(“C”, 1, 2.0); //还要传入参数
-	b->Func();
-}
-
-*/
